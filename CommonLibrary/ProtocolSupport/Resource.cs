f62ca1d81d196998ecc26b833ee80091349753ab
Poppy Lin <145313938+poppylin2@users.noreply.github.com>
2024-08-26 01:15:53 -0700

    Completed login windows for both the server and the client, implemented the main functions

---
+++ CommonLibrary/ProtocolSupport/Resource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonLibrary
+{
+    /// <summary>
+    /// All Data Repositories
+    /// </summary>
+    public class SoftResources
+    {
+        /// <summary>
+        /// All string Repositories
+        /// </summary>
+        public class StringResouce
+        {
+            public static string SoftName { get; } = "Welcome to InfoHubX";
+            public static string SoftCopyRight { get; } = "©️Poppy";
+
+
+
+            public const string AccountLoadFailed = "Failed to add account";
+            public const string AccountDeleteSuccess = "Account deleted:";
+            public const string AccountAddSuccess = "Account added:";
+            public const string AccountModifyPassword = "Account password changed:";
+            public const string AccountUploadPortrait = "Account portrait updated:";
+
+        }
+    }
+}
---
+++ InfoSystemServer/UserServer.cs
@@ -0,0 +1,72 @@
+﻿using CommonLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//=========================================================================================
+//
+// Template description: Uses the BasicFramework and network framework to implement a server template, including basic creation operations.
+//
+//=========================================================================================
+
+
+
+namespace 软件系统服务端模版
+{
+    /// <summary>
+    /// Server class that stores static parameters for system operation.
+    /// </summary>
+    public class UserServer
+    {
+        /// <summary>
+        /// Version number, announcement, whether login is allowed, and login restriction storage
+        /// </summary>
+        public static ServerSettings ServerSettings { get; set; } = new ServerSettings();
+
+        /// <summary>
+        /// Storage object for all account information. The specific account class can be extended based on UserAccount.
+        /// </summary>
+        public static ServerAccounts<UserAccount> ServerAccounts { get; set; } = new ServerAccounts<UserAccount>(
+            new List<UserAccount>()
+            {
+                // eg: add a default super admin
+                new UserAccount()
+                {
+                    UserName="admin",
+                    Password="123456",
+                    Factory="Headquarters",
+                    RegisterTime=DateTime.Now,
+                    LastLoginTime=DateTime.Now,
+                    LoginEnable=true,
+                    Grade=AccountGrade.SuperAdministrator,
+                    ForbidMessage="This account has been disabled",
+                    LoginFrequency=0,
+                    LastLoginIpAddress="",
+                }
+            }
+        );
+
+        /// <summary>
+        /// Role info management
+        /// </summary>
+        public static RoleAssign ServerRoles { get; set; } = new RoleAssign();
+
+    }
+
+    /// <summary>
+    /// An extended user account example, which can replace the server and client account classes.
+    /// It can also be directly extended in the source code.
+    /// </summary>
+    public class UserAccountEx : UserAccount
+    {
+        /// <summary>
+        /// Age
+        /// </summary>
+        public int Age { get; set; } = 0;
+        /// <summary>
+        /// Home Address
+        /// </summary>
+        public string HomeAddress { get; set; } = "";
+    }
+}
